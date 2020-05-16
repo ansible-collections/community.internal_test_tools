a3952a721a3e8ecd53f213d0d5c8b365ac234ad1
Felix Fontein <felix@fontein.de>
2020-05-16 16:10:41 +0200

    Add extra sanity tests: changelog fragment validation and unwanted files checking.

--- tests/utils/shippable/sanity.sh
+++ tests/utils/shippable/sanity.sh
@@ -2,10 +2,20 @@
 
 set -o pipefail -eux
 
+declare -a args
+IFS='/:' read -ra args <<< "$1"
+
+group="${args[1]}"
+
 if [ "${BASE_BRANCH:-}" ]; then
     base_branch="origin/${BASE_BRANCH}"
 else
     base_branch=""
+fi
+
+if [ "${group}" == "extra" ]; then
+    tools/run.py --color
+    exit
 fi
 
 # shellcheck disable=SC2086
