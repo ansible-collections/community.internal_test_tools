0cfd4cd88b0a1e2095d6c9af4523d9f4e2ea3ff5
Felix Fontein <felix@fontein.de>
2021-11-14 12:32:04 +0100

    Bump default Docker container fallback.

---
+++ tests/unit/tools/test_run.py
@@ -0,0 +1,26 @@
+# (c) 2020 Felix Fontein <felix@fontein.de>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from distutils.version import LooseVersion
+
+import pytest
+
+from ansible.release import __version__ as ansible_core_version
+
+from ansible_collections.community.internal_test_tools.tools.run import (
+    get_default_container_2_12,
+    get_default_container_pre_2_12,
+)
+
+
+@pytest.mark.skipif(LooseVersion(ansible_core_version) < LooseVersion('2.12'))
+def test_get_default_container_2_12():
+    assert get_default_container_2_12() is not None
+
+
+@pytest.mark.skipif(LooseVersion(ansible_core_version) >= LooseVersion('2.12'))
+def test_get_default_container_pre_2_12():
+    assert get_default_container_pre_2_12() is not None
