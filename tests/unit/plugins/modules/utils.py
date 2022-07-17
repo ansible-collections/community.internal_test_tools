fb9ccfe79811251e48fec148d7b878249c1f5ce4
Felix Fontein <felix@fontein.de>
2022-07-17 13:10:03 +0200

    Fix/add more copyright statements.

--- tests/unit/plugins/modules/utils.py
+++ tests/unit/plugins/modules/utils.py
@@ -1,4 +1,4 @@
-# (c) 2020 Ansible
+# Copyright (c) 2020 Ansible
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
