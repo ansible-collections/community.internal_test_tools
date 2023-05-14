9400eb0fd63a60fc843b14ca1d255c40e46b0f47
Felix Fontein <felix@fontein.de>
2023-05-14 12:27:20 +0200

    Remove deprecated meta/runtime.yml tool (#91)
    * Remove deprecated meta/runtime.yml tool.
    
    * Remove tests.

--- tools/lib/meta_runtime.py
+++ tools/lib/meta_runtime.py
@@ -60,23 +60,6 @@ def scan_file_redirects(redirects, collection_root='.', remove=False):
                         os.unlink(path)
 
 
-def scan_flatmap_redirects(redirects, collection_root='.'):
-    for plugin_type in ('action', 'modules'):
-        plugin_redirects = redirects[plugin_type]
-        base_dir = os.path.join(collection_root, 'plugins', plugin_type)
-        for root, dirnames, filenames in os.walk(base_dir):
-            if root == base_dir:
-                continue
-            for filename in filenames:
-                if not filename.endswith('.py'):
-                    continue
-                if filename in ('__init__', ):
-                    continue
-                source = path_to_name(os.path.join(base_dir, filename), base_dir)
-                destination = path_to_name(os.path.join(root, filename), base_dir)
-                record_redirect(plugin_type, plugin_redirects, source, destination)
-
-
 def scan_plugins(plugins, redirects, runtime, collection_root='.', all_plugins=False):
     plugin_routing = runtime.get('plugin_routing') or {}
     for plugin_type in PLUGIN_TYPES:
@@ -104,26 +87,6 @@ def scan_plugins(plugins, redirects, runtime, collection_root='.', all_plugins=F
                     plugins_set.add(plugin_name)
 
 
-def name_to_path(name, base_dir):
-    return os.path.join(base_dir, name.replace('.', os.path.sep) + '.py')
-
-
-def add_file_redirects(redirects, collection_root='.'):
-    for plugin_type in PLUGIN_TYPES:
-        if plugin_type in ('test', 'filter'):
-            # Test and filter plugins are not coupled to filenames
-            continue
-        base_dir = os.path.join(collection_root, 'plugins', plugin_type)
-        for source, destination in redirects[plugin_type].items():
-            src = name_to_path(source, base_dir)
-            dst = name_to_path(destination, base_dir)
-            rel_dst = os.path.normpath(os.path.relpath(dst, os.path.dirname(src)))
-            if os.path.islink(src):
-                if os.path.normpath(os.readlink(src)) == rel_dst:
-                    continue
-            os.symlink(rel_dst, src)
-
-
 def extract_meta_redirects(redirects, runtime, collection_name, remove=False):
     plugin_routing = runtime.get('plugin_routing') or {}
     collection_prefix = '{name}.'.format(name=collection_name)
@@ -139,35 +102,6 @@ def extract_meta_redirects(redirects, runtime, collection_name, remove=False):
                         redirect[len(collection_prefix):])
                     if remove and plugin_type not in ('test', 'filter'):
                         del plugin_data['redirect']
-
-
-def add_meta_redirects(redirects, runtime, collection_name):
-    plugin_routing = runtime.get('plugin_routing')
-    for plugin_type in PLUGIN_TYPES:
-        for source, destination in redirects[plugin_type].items():
-            if plugin_routing is None:
-                runtime['plugin_routing'] = plugin_routing = dict()
-            if plugin_type not in plugin_routing:
-                plugin_routing[plugin_type] = dict()
-            if source not in plugin_routing[plugin_type]:
-                plugin_routing[plugin_type][source] = dict()
-            plugin_routing[plugin_type][source]['redirect'] = (
-                '{collection_name}.{destination}'.format(
-                    collection_name=collection_name,
-                    destination=destination,
-                )
-            )
-
-
-def sort_plugin_routing(runtime):
-    plugin_routing = runtime.get('plugin_routing')
-    if not plugin_routing:
-        return
-    for plugin_type in PLUGIN_TYPES:
-        plugins = plugin_routing.get(plugin_type)
-        if not plugins:
-            continue
-        plugin_routing[plugin_type] = dict(sorted(plugins.items()))
 
 
 def load_ansible_core_runtime():
