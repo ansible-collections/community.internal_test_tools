f79cc3148ddab378a4732c6d4dded17e1c7f337c
Felix Fontein <felix@fontein.de>
2023-11-11 11:58:30 +0100

    Extra sanity test runner: improve how to run pip (#104)
    * Use 'python -m pip' instead of 'pip' directly.
    
    * Run pip with PIP_BREAK_SYSTEM_PACKAGES=1.
    
    * Add changelog.
    
    * Forgot comma.

--- tools/runner.py
+++ tools/runner.py
@@ -297,12 +297,17 @@ def setup(tests, use_color=True):
         print(colorize('Installing requirements', 'emph', use_color))
     for python_version, reqs in sorted(requirements.items()):
         command = [
-            'pip{0}'.format(python_version),
+            'python{0}'.format(python_version),
+            '-m',
+            'pip',
             'install',
             '--disable-pip-version-check'
         ] + reqs
+        env = os.environ.copy()
+        # Allow to break system packages (PEP-668). Since we have an isolated environment, this shouldn't be a problem
+        env['PIP_BREAK_SYSTEM_PACKAGES'] = '1'
         print(colorize('Running {0}'.format(join_command(command)), 'emph', use_color))
-        process_result = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process_result = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
         stdout, stderr = process_result.communicate()
         if stdout:
             for line in stdout.decode('utf-8').splitlines():
