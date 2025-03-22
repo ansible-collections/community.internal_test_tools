176b61a9edcee090ea1d2a11b82ee3f5030918bf
Felix Fontein <felix@fontein.de>
2025-03-22 11:42:57 +0100

    Add support for DT's WarningSummary.

--- tests/unit/plugins/modules/test_utils.py
+++ tests/unit/plugins/modules/test_utils.py
@@ -5,7 +5,18 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import pytest
+
 from .utils import extract_warnings_texts
+
+try:
+    from ansible.module_utils.common.messages import (
+        WarningSummary as _WarningSummary,
+        Detail as _Detail,
+    )
+except ImportError:
+    _WarningSummary = None
+    _Detail = None
 
 
 def test_extract_warnings_texts_1():
@@ -13,3 +24,9 @@ def test_extract_warnings_texts_1():
     assert extract_warnings_texts({'warnings': None}) == []
     assert extract_warnings_texts({'warnings': []}) == []
     assert extract_warnings_texts({'warnings': ['foo']}) == ['foo']
+
+
+@pytest.mark.skipif(_WarningSummary is None or _Detail is None, reason="ansible-core does not support Data Tagging")
+def test_extract_warnings_texts_2():
+    assert extract_warnings_texts({'warnings': [_WarningSummary(details=(_Detail(msg='foo'), ))]}) == ['foo']
+    assert extract_warnings_texts({'warnings': [_WarningSummary(details=(_Detail(msg='foo'), _Detail(msg='bar')))]}) == ['foo']
